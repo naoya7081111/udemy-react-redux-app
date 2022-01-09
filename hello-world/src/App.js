c5ee7717d8a7edb88580b4a256838cde805a8dd8
naoya7081111 <naoya708111@gmail.com>
2022-01-09 09:21:31 +0900

    1/9

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,10 +1,20 @@
+import React from "react";
+
 function App() {
+  const dom = <h1>Hi!</h1>;
+  const greeting = "Hi!, TOM";
+
+  // return dom
   return (
-    <h1>Hello World!!</h1>
+    <>
+      <h1 className="who">Hello World!!</h1>
+      <h2>{greeting}</h2> 
+      <label htmlFor="bar">bar</label>
+      <input type="text" id='bar' onChange={ () => console.log("yeah!") }/>
+    </>
   );
 }
 
-// import React from "react";
 
 // function App() {
 //   return React.createElement(
