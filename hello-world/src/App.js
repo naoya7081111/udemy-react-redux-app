c543710f1d3f1cda61e663e57e55413e43f15a4c
naoya7081111 <naoya708111@gmail.com>
2022-01-09 10:05:34 +0900

    1/9

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -4,26 +4,36 @@ function App() {
   const dom = <h1>Hi!</h1>;
   const greeting = "Hi!, TOM";
 
+  const profiles = [
+    {name: "Taro", age:"10"},
+    {name:"Hanako", age:"5"},
+    {name:"kanako"}
+  ]
+
   // return dom
   return (
     <>
-      <h1 className="who">Hello World!!</h1>
+      {/* <h1 className="who">Hello World!!</h1>
       <h2>{greeting}</h2> 
       <label htmlFor="bar">bar</label>
-      <input type="text" id='bar' onChange={ () => console.log("yeah!") }/>
-      <Cat />
-      <Cat />
-      <Cat />
-    </>
+      <input type="text" id='bar' onChange={ () => console.log("yeah!") }/> */}
+      {profiles.map((profile, index) => {
+        return <User name={profile.name} age={profile.age} key={index} />
+      })}
+          </>
   );
 }
 
-const Cat = () => {
+const User = (props) => {  
   return (
     <>
-      <div>Mew!</div>
+      <div>I am {props.name}, and {props.age} years old!</div>
     </>
   )
+}
+
+User.defaultProps = {
+  age: 1
 }
 
 
