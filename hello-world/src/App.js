2ad9a9e3c13743e9db89e86c42c416f9f9577329
naoya7081111 <naoya708111@gmail.com>
2022-01-15 10:46:28 +0900

    1/15_state

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,46 +1,47 @@
-import React from "react";
-import PropTypes from "prop-types";
+import React, { Component } from "react";
+// import PropTypes from "prop-types";
 
-function App() {
-  const dom = <h1>Hi!</h1>;
-  const greeting = "Hi!, TOM";
+// function App() {
+//   const dom = <h1>Hi!</h1>;
+//   const greeting = "Hi!, TOM";
 
-  const profiles = [
-    {name: "Taro", age:10},
-    {name:"Hanako", age:5},
-    {name:"NoName"}
-  ]
+//   const profiles = [
+//     {name: "Taro", age:10},
+//     {name:"Hanako", age:5},
+//     {name:"NoName"}
+//   ]
 
-  // return dom
-  return (
-    <>
-      {/* <h1 className="who">Hello World!!</h1>
-      <h2>{greeting}</h2> 
-      <label htmlFor="bar">bar</label>
-      <input type="text" id='bar' onChange={ () => console.log("yeah!") }/> */}
-      {profiles.map((profile, index) => {
-        return <User name={profile.name} age={profile.age} key={index} />
-      })}
-          </>
-  );
-}
+//   return dom
+//   return (
+//     <>
+//       {/* <h1 className="who">Hello World!!</h1>
+//       <h2>{greeting}</h2> 
+//       <label htmlFor="bar">bar</label>
+//       <input type="text" id='bar' onChange={ () => console.log("yeah!") }/> */}
+//       {profiles.map((profile, index) => {
+//         return <User name={profile.name} age={profile.age} key={index} />
+//       })}
+//           </>
+//   );
 
-const User = (props) => {  
-  return (
-    <>
-      <div>I am {props.name}, and {props.age} years old!</div>
-    </>
-  )
-}
+// }
 
 // User.defaultProps = {
 //   age: 1
 // }
 
-User.propTypes = {
-  name: PropTypes.string.isRequired,
-  age: PropTypes.number
-}
+// const User = (props) => {  
+//   return (
+//     <>
+//       <div>I am {props.name}, and {props.age} years old!</div>
+//     </>
+//   )
+// }
+
+// User.propTypes = {
+//   name: PropTypes.string.isRequired,
+//   age: PropTypes.number
+// }
 
 // function App() {
 //   return React.createElement(
@@ -49,5 +50,36 @@ User.propTypes = {
 //     "Hello World!!"
 //   );
 // }
+
+
+const App = () => (<Counter></Counter>)
+
+
+class Counter extends Component { 
+  constructor(props) {
+    super(props)
+    console.log(this.state);
+    this.state = { count: 0 };
+  }
+
+  render() {
+    const onClickAdd = () => {
+      this.setState({ count: this.state.count + 1 })
+    }  
+    const onClickReduce = () => {
+      this.state.count !== 0 && this.setState({ count: this.state.count - 1 })
+    }  
+
+    return (
+      // React.Fragmentは<></>と同じ
+      <React.Fragment>
+        <div>count: { this.state.count }</div>
+        <button onClick={onClickAdd}>+1</button>
+        <button onClick={onClickReduce} >-1</button>
+      </React.Fragment>
+    )
+  }
+}
+
 
 export default App;
