bf374068cb12af75d7920861bd5a35e66a18ad62
naoya7081111 <naoya708111@gmail.com>
2022-01-23 10:06:48 +0900

    store

--- hello-world/src/index.js
+++ hello-world/src/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import reducers from '../../reducers';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const store = createStore(reduce);
+
 ReactDOM.render(
   <React.StrictMode>
+    <Provider store={store}>
     <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
